9a118fef0240de074709cec37a2acacb652b3b65
Tobias Brodd <tobias.brodd@gmail.com>
2021-04-05 18:47:28 +0200

    added logging middleware

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -13,21 +17,34 @@ type Data struct {
 
 var data []Data
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func renderJSON(w http.ResponseWriter, v interface{}) {
+	js, err := json.Marshal(v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
+	w.Write(js)
 }
 
-func router() *mux.Router {
-	r := mux.NewRouter()
-	r.HandleFunc("/", handler).Methods("GET")
-	return r
+func handler(w http.ResponseWriter, r *http.Request) {
+	renderJSON(w, data)
 }
 
 func main() {
-	router := router()
+	router := mux.NewRouter()
+	router.StrictSlash(true)
 
 	data = append(data, Data{Content: "Some content"})
+	router.HandleFunc("/", handler).Methods("GET")
 
-	http.ListenAndServe(":8000", router)
+	router.Use(func(h http.Handler) http.Handler {
+		return handlers.LoggingHandler(os.Stdout, h)
+	})
+	router.Use(handlers.RecoveryHandler(handlers.PrintRecoveryStack(true)))
+
+	serverDomain := "localhost"
+	serverPort := 8000
+	log.Printf("Listening at %s:%d", serverDomain, serverPort)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", serverDomain, serverPort), router))
 }
